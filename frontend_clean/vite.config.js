daf67e4e10f27d6c6c39fa6c927d672780ed62b2
root <root@DESKTOP-0LKVF9R.>
2025-06-09 00:44:05 +0300

    Fix vite.config.js - remove unused plugin

--- frontend_clean/vite.config.js
+++ frontend_clean/vite.config.js
@@ -1,23 +1,13 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import rollupNodePolyFill from 'rollup-plugin-node-polyfills';
 
 export default defineConfig({
-  plugins: [
-    react(),
-    rollupNodePolyFill() // fallback dacă ai nevoie de polyfill-uri
-  ],
+  plugins: [react()],
   optimizeDeps: {
     esbuildOptions: {
       define: {
         global: 'globalThis',
       },
-    },
-  },
-  resolve: {
-    alias: {
-      buffer: 'buffer',
-      stream: 'stream-browserify',
     },
   },
 });
